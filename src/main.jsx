781ddc61f91d2340caa8ad75a76e31b1fcb8f0c7
ivanosettembri-jpg <ivanosettembri@gmail.com>
2026-04-24 02:35:10 +0200

    test deploy

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TheCabal from './TheCabal.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TheCabal />
+  </React.StrictMode>
+)
