b7d1d25bd44c4bfa2a872b6b8bdd277d1c9f520a
ivanosettembri-jpg <ivanosettembri@gmail.com>
2026-04-30 13:06:12 +0200

    53

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'inject-firebase-key',
-      transformIndexHtml: (html) =>
-        html.replace('__FIREBASE_API_KEY__', process.env.VITE_FIREBASE_API_KEY || ''),
-    }
-  ],
+  plugins: [react()],
   build: { chunkSizeWarningLimit: 1000 },
 })
